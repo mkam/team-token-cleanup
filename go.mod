7c181eca4e26d0f5f61911b5d11343785dc87b9e
Melissa Kam <mkam@hashicorp.com>
2025-05-16 18:58:34 -0500

    Script for deleting team tokens in HCP Terraform

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/mkam/team-token-cleanup
+
+go 1.22.2
+
+require github.com/hashicorp/go-tfe v1.80.1-0.20250516184651-8d9f805c67ac
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
+	github.com/hashicorp/go-slug v0.16.4 // indirect
+	github.com/hashicorp/go-version v1.7.0 // indirect
+	github.com/hashicorp/jsonapi v1.4.3-0.20250220162346-81a76b606f3e // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/time v0.10.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-tfe"
+)
+
+func main() {
+	ctx := context.Background()
+
+	delete := flag.Bool("delete", false, "Deletes the team tokens that fit the provided criteria for deletion. Defaults to false.")
+	team := flag.String("team", "", "The team name to delete tokens for. If not provided, tokens from all teams will be considered for deletion.")
+	deleteExpired := flag.Bool("expired", true, "Marks expired tokens for deletion, regardless of created_at or last_used_at.")
+	lastUsedAt := flag.Int("last-used-days-ago", 30, "Duration of time in days for how long ago a resource should have been "+
+		"last used before deleting.")
+	createdAt := flag.Int("created-at-days-ago", 0, "Duration of time in days for how long ago a resource should have been "+
+		"created before deleting.")
+	flag.Parse()
+
+	config := &tfe.Config{
+		RetryServerErrors: true,
+	}
+
+	client, err := tfe.NewClient(config)
+	if err != nil {
+		fmt.Println("error initializing TFE client:", err)
+		os.Exit(1)
+	}
+
+	orgName := os.Getenv("TFE_ORGANIZATION")
+	if orgName == "" {
+		fmt.Println("TFE_ORGANIZATION environment variable is not set.")
+		os.Exit(1)
+	}
+
+	opts := &tfe.TeamTokenListOptions{
+		ListOptions: tfe.ListOptions{
+			PageSize: 100,
+		},
+	}
+	if *team != "" {
+		opts.Query = *team
+	}
+	tokens, err := client.TeamTokens.List(ctx, orgName, opts)
+	if err != nil {
+		fmt.Printf("error listing team tokens for organization %s: %v\n", orgName, err)
+		os.Exit(1)
+	}
+
+	var createdAtDuration time.Duration
+	if *createdAt > 0 {
+		createdAtDuration = time.Duration(*createdAt*24) * time.Hour
+	}
+
+	var lastUsedAtDuration time.Duration
+	if *lastUsedAt > 0 {
+		lastUsedAtDuration = time.Duration(*lastUsedAt*24) * time.Hour
+	}
+
+	now := time.Now()
+	toDelete := make([]*tfe.TeamToken, 0)
+	for _, token := range tokens.Items {
+
+		if *deleteExpired && !token.ExpiredAt.IsZero() && token.ExpiredAt.Before(now) {
+			fmt.Printf("Marking token for deletion because expired: %s %s expired_at=%s \n", token.ID, token.Team.ID, token.ExpiredAt.String())
+			toDelete = append(toDelete, token)
+			continue
+		}
+
+		if lastUsedAtDuration > 0 && time.Since(token.LastUsedAt) > lastUsedAtDuration {
+			fmt.Printf("Marking token for deletion because last used too long ago: %s %s last_used_at=%s \n", token.ID, token.Team.ID, token.LastUsedAt.String())
+			toDelete = append(toDelete, token)
+			continue
+		}
+
+		if createdAtDuration > 0 && time.Since(token.CreatedAt) > createdAtDuration {
+			fmt.Printf("Marking token for deletion because created too long ago: %s %s created_at=%s \n", token.ID, token.Team.ID, token.CreatedAt.String())
+			toDelete = append(toDelete, token)
+			continue
+		}
+
+	}
+
+	fmt.Printf("\n%d tokens marked for deletion.\n", len(toDelete))
+	if !*delete {
+		fmt.Println("Use the --delete flag to delete the tokens that fit the specified criteria.")
+		os.Exit(0)
+	}
+
+	if len(toDelete) == 0 {
+		os.Exit(0)
+	}
+
+	r := bufio.NewReader(os.Stdin)
+	fmt.Println("Are you sure you want to delete these team tokens? (y/n): ")
+	resp, err := r.ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		os.Exit(1)
+	}
+
+	if strings.ToLower(strings.TrimSpace(resp)) != "y" && strings.ToLower(strings.TrimSpace(resp)) != "yes" {
+		fmt.Println("Aborting deletion.")
+		os.Exit(0)
+	}
+
+	for _, token := range toDelete {
+		fmt.Println("Deleting token: ", token.ID)
+		err := client.TeamTokens.DeleteByID(ctx, token.ID)
+		if err != nil {
+			fmt.Printf("Error deleting token %s: %v\n", token.ID, err)
+			continue
+		}
+	}
+	fmt.Println("Team tokens deleted.")
+}
